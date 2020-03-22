d0524b61b50f7c33d71a9429127019627a00eadb
kastrubait <60570588+kastrubait@users.noreply.github.com>
2020-03-22 17:32:09 +0300

    add solution transform-array
    add solution transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,30 @@
 module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
+    //throw 'Not implemented';
     // remove line with error and write your code here
+    let note = Object.prototype.toString.call(arr);
+    
+    if ( note !== [Object]) {
+    let res = arr.slice();
+    for (let i = res.length-1; i >= 0; i--) {
+        note = res[i];
+        switch (note) {
+            case '--discard-next':
+                (i+1 <= res.length-1) ? res = res.splice(i, 2): res = res.splice(i, 0);
+                break;
+            case '--discard-prev':
+                (i-1 >= 0) ? res = res.splice(i-1, 2): res = res.splice(i, 0);
+                res = res.splice(i-1, 2);
+                break;
+            case '--double-next':
+                (i+1 <= res.length-1) ? res = res.splice(i,1,res[i]): res = res.splice(i, 0);
+                break;
+            case '--double-prev':
+                (i-1 >= 0) ? res = res.splice(i,1, res[i-1]): res = res.splice(i, 0);
+                break;
+        }
+    }
+    return res;
+    } else throw new RangeError('Error');
+};
+
 };
