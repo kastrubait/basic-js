afb29a0c464973c442bc0d83c80482a3820ad6fd
kastrubait <60570588+kastrubait@users.noreply.github.com>
2020-03-22 18:52:47 +0300

    add solution dream-team
    add solution dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,13 @@
 module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
+  //throw 'Not implemented';
   // remove line with error and write your code here
+  let team = [];
+  for (let el of members) {
+      if (typeof el == 'string') {
+         el = el.replace(/^\s*/,'');
+         team = team.concat(el[0]);
+        }
+  }
+  team = team.map(item => item.toUpperCase());
+  return  team.sort().join('');
 };
