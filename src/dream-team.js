803a743b06bcf5d35fb6f391ec4fbe8318c220dd
kastrubait <60570588+kastrubait@users.noreply.github.com>
2020-10-02 22:39:37 +0300

    fix: update dreame-time.js
    Update dreame-time.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,13 +1,16 @@
-module.exports = function createDreamTeam(/* members */) {
-  //throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+ 
+  if ( ! (members != null && typeof members[Symbol.iterator] === 'function')) {
+    return false;
+  }
   let team = [];
   for (let el of members) {
-      if (typeof el == 'string') {
+      if (typeof el === 'string') {
          el = el.replace(/^\s*/,'');
          team = team.concat(el[0]);
-        }
+        } 
   }
   team = team.map(item => item.toUpperCase());
   return  team.sort().join('');
+
 };
