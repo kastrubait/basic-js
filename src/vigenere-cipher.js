3be9c2d76a4cbc0fe15aef836a5574002cf0a20a
kastrubait <60570588+kastrubait@users.noreply.github.com>
2020-10-03 11:03:55 +0300

    add: Add solution vigenere-cipher.js
    Add solution vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,42 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+   
+    constructor(data = true) {
+        this.data = data;
+        this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+        if(!message || !key) throw new Error();
+        let result = [];
+        let count = 0;
+        for (let i = 0; i < message.length; i++) {
+            const position = this.alphabet.indexOf(message[i].toUpperCase())
+            if (position !== -1) {
+                const shift = this.alphabet.indexOf(key[count % key.length].toUpperCase());
+                count++;
+                result.push(this.alphabet[(position + shift) % this.alphabet.length])
+            } else {
+                result.push(message[i])
+            }  
+        }
+        return this.data ? result.join('') : result.reverse().join('');
+    }
+
+    decrypt(message, key) {
+        if(!message || !key) throw new Error();
+        let result = [];
+        let count = 0;
+        for (let i = 0; i < message.length; i++) {
+            const position = this.alphabet.indexOf(message[i].toUpperCase())
+            if (position !== -1) {
+                const shift = this.alphabet.indexOf(key[count % key.length].toUpperCase());
+                count++;
+                result.push(this.alphabet.slice((position - shift), (position - shift + 1)))
+            } else {
+                result.push(message[i])
+            }  
+        }
+        return this.data ? result.join('') : result.reverse().join('');
     }
 }
 
