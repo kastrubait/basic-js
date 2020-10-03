d7f6b045a1ad3c52687c6395db61715e66e6148a
kastrubait <60570588+kastrubait@users.noreply.github.com>
2020-10-03 11:49:56 +0300

    add: Add solution carbon-darting.js
    Add solution carbon-darting.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,6 +2,8 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+   if (typeof sampleActivity !== 'string' ||  isNaN(parseFloat(sampleActivity)) ||  parseFloat(sampleActivity) <= 0 || parseFloat(sampleActivity) > MODERN_ACTIVITY) {
+    return false;
+  }
+  return Math.ceil((Math.log(MODERN_ACTIVITY/parseFloat(sampleActivity)))/(0.693/HALF_LIFE_PERIOD));
 };
