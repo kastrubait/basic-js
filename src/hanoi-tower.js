fc3cbf01c68811ec955671c44efd413d0b3f9e73
kastrubait <60570588+kastrubait@users.noreply.github.com>
2020-10-20 06:30:43 +0300

    Update hanoi-tower.js
    add rounding result

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -3,6 +3,6 @@ module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
     // remove line with error and write your code here
     let result = {};
     result.turns = Math.pow(2, disksNumber) -1;
-    result.seconds = result.turns/(turnsSpeed/3600);
+    result.seconds =  Math.floor(result.turns/(turnsSpeed/3600));
     return result;
 }
