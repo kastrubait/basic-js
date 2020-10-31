966c066aeb581f1b02021bfa225f68cc539a509e
kastrubait <60570588+kastrubait@users.noreply.github.com>
2020-11-01 00:24:07 +0300

    Update transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -6,25 +6,31 @@ module.exports = function transform(arr) {
     let length = arr.length;
     let transformArray = [];
     for (let i = 0; i < length; i++) {
-        if (arr[i] === '--double-next') {
-            if (length - 1 > i) {
-                transformArray.push(arr[i + 1]);
-            }
-        } else if (arr[i] === '--double-prev') {
-            if (i > 0) {
-                transformArray.push(arr[i - 1]);
-            }
-        } else if (arr[i] === '--discard-prev') {
-            if (i > 0) {
-                transformArray.pop();
-            }
-        } else if (arr[i] === '--discard-next') {
-            if (length - 1 > i) {
+         switch (arr[i]) {
+            case '--discard-next':
                 i++;
-            }
-        } else {
-            transformArray.push(arr[i]);
-        }
-    }
+                break;
+            case '--discard-prev':
+                if (i > 0 && arr[i - 2] !== '--discard-next') {
+                    transformArray.pop();
+                }
+                i;
+                break;
+            case '--double-next':
+                if (i < arr.length - 1) {
+                    transformArray.push(arr[i + 1]);
+                }
+                i;
+                break;
+            case '--double-prev':
+                if (i > 0 && arr[i - 2] !== '--discard-next') {
+                    transformArray.push(arr[i - 1]);
+                }
+                i;
+                break;
+            default:
+                transformArray.push(arr[i]);
+        };
+    };
     return transformArray;
 };
